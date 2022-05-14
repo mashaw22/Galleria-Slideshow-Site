09882f6209bac78e359ec50f34a6a57b78a58a14
Samantha Neely <samneely.dev@gmail.com>
2022-05-13 23:08:44 -0500

    added pagination to next and back buttons

--- src/components/GalleryDirections.js
+++ src/components/GalleryDirections.js
@@ -1,18 +1,25 @@
+import { Link } from 'gatsby'
 import React from 'react'
 import iconBackButton from "../images/icons/iconBackButton.svg"
 import iconNextButton from "../images/icons/iconNextButton.svg"
-import {directions, text, images} from "../styles/directions.module.css"
+import * as styles from "../styles/directions.module.css"
 
-export default function GalleryDirections({ name, artist }) {
+export default function GalleryDirections({ name, artist, previous, next }) {
+
   return (
-    <div className={directions}>
-        <div className={text}>
+    <div className={styles.directions}>
+        <div className={styles.text}>
             <h3>{name}</h3>
             <p>{artist}</p>
         </div>
-        <div className={images}>
-            <img src={iconBackButton} alt="back button"/>
-            <img src={iconNextButton} alt="next button"/>
+        <div className={styles.images}>
+            {previous && <Link to={"/" + previous.frontmatter.slug} rel="prev">
+              <img className={styles.backButton}src={iconBackButton} alt="back button"/>
+            </Link>}
+
+            {next && <Link to={"/" + next.frontmatter.slug} rel="next">
+              <img className={styles.nextButton} src={iconNextButton} alt="next button"/>
+            </Link>}
         </div>
     </div>
   )
--- src/templates/painting-details.js
+++ src/templates/painting-details.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import GalleryDirections from '../components/GalleryDirections'
@@ -6,65 +6,76 @@ import Layout from "../components/Layout"
 import iconViewImage from "../images/icons/iconViewImage.svg"
 import * as styles from "../styles/painting-details.module.css"
 
-export default function PaintingDetails({ data }) {
-  const { html } = data.markdownRemark
-  const {name, source, year, artist, images} = data.markdownRemark.frontmatter
-  const [imageIsShown, setImageIsShown] = useState(false)
-
-  function viewImage(){
-    setImageIsShown(true)
+class PaintingDetails extends React.Component {
+  state = {
+    imageIsShown: false
+  }
+  viewImage = () => {
+    this.setState({imageIsShown: true})
   }
 
-  function hideImage(){
-    setImageIsShown(false)
+  hideImage = () => {
+    return this.setState({imageIsShown: false})
   }
+  render() {
 
-  return (
-      <Layout stopSlideShow="Stop Slideshow">
-        <div className={styles.layout}>
-
-          <div className={styles.hero__section}>
-            <div>
-              <GatsbyImage className={styles.hero__small} image={getImage(images.hero.small)} alt={name}/>
-              <GatsbyImage className={styles.hero__large}image={getImage(images.hero.large)} alt={name}/>
-              <button className={styles.hero__view} onClick={viewImage}>
-                <img src={iconViewImage} alt="View"/>
-                View Image
-              </button>
-            </div>
-
-              <div className={styles.hero__subdetails}>
-                <div className={styles.hero__text}>
-                    <h1>{name}</h1>
-                    <h2>{artist.name}</h2>
-                </div>    
-                <div className={styles.hero__artist}>
-                  <GatsbyImage image={getImage(artist.image)} alt={artist.name} />
-                </div>
+    console.log(this.props.data)
+    const { html } = this.props.data.markdownRemark
+    const {name, source, year, artist, images} = this.props.data.markdownRemark.frontmatter
+    const { previous, next } = this.props.pageContext
+    console.log(previous)
+    console.log(next)
+  
+  
+    return (
+        <Layout stopSlideShow="Stop Slideshow">
+          <div className={styles.layout}>
+  
+            <div className={styles.hero__section}>
+              <div>
+                <GatsbyImage className={styles.hero__small} image={getImage(images.hero.small)} alt={name}/>
+                <GatsbyImage className={styles.hero__large}image={getImage(images.hero.large)} alt={name}/>
+                <button className={styles.hero__view} onClick={this.viewImage}>
+                  <img src={iconViewImage} alt="View"/>
+                  View Image
+                </button>
               </div>
-          </div>
-          
-          <div className={styles.details}>
-            <h3>{year}</h3>
-            <div className={styles.details__text}>
-              <div dangerouslySetInnerHTML={{ __html: html}}/>
-              <a href={source}>Go to source</a>-[]
+  
+                <div className={styles.hero__subdetails}>
+                  <div className={styles.hero__text}>
+                      <h1>{name}</h1>
+                      <h2>{artist.name}</h2>
+                  </div>    
+                  <div className={styles.hero__artist}>
+                    <GatsbyImage image={getImage(artist.image)} alt={artist.name} />
+                  </div>
+                </div>
             </div>
-
-          </div>
-        </div>
-        <GalleryDirections name={name} artist={artist.name}/>
-        {imageIsShown && 
-          <div className={styles.overlay}>
-            <div className={styles.overlay__main}>
-              <GatsbyImage className={styles.overlay__imageSmall} image={getImage(images.hero.small)} alt={name} />
-              <GatsbyImage className={styles.overlay__imageLarge} image={getImage(images.hero.large)} alt={name} />
-              <button onClick={hideImage}className={styles.overlay__button}>Close</button>
+            
+            <div className={styles.details}>
+              <h3>{year}</h3>
+              <div className={styles.details__text}>
+                <div dangerouslySetInnerHTML={{ __html: html}}/>
+                <a href={source}>Go to source</a>
+              </div>
+  
             </div>
-          </div>}
-      </Layout>
-  )
+          </div>
+          <GalleryDirections name={name} artist={artist.name} previous={previous} next={next}/>
+          {this.state.imageIsShown && 
+            <div className={styles.overlay}>
+              <div className={styles.overlay__main}>
+                <GatsbyImage className={styles.overlay__imageSmall} image={getImage(images.hero.small)} alt={name} />
+                <GatsbyImage className={styles.overlay__imageLarge} image={getImage(images.hero.large)} alt={name} />
+                <button onClick={this.hideImage}className={styles.overlay__button}>Close</button>
+              </div>
+            </div>}
+        </Layout>
+    )
+  }
 }
+
+export default PaintingDetails
 
 export const query = graphql`
   query PaintingDetails($slug: String) {
